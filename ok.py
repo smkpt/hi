a0a95739a05292fcd0582491d124c5d4f4ef7cf4
smkpt <qaauto2013@gmail.com>
2019-06-14 23:05:36 +0530

    pytest sample

--- ok.py
+++ ok.py
@@ -1,3 +1,14 @@
 print ("Hello Python from git")
 print ("Hello Python from git Hello")
 
+import pytest
+
+
+def test_func_fast():
+    pass
+
+
+@pytest.mark.slow
+def test_func_slow():
+    pass  
+
